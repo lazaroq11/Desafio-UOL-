f3946d7a439d7b03bbf8ee8b2a240ff3755d636f
lazaro-silva <lazaro.silva@hp.com>
2023-04-25 01:35:39 -0300

    fix: include stopPropagation event in ex4.js for stop the redirect event in favorited sign

--- logica/ex4.js
+++ logica/ex4.js
@@ -1,11 +1,13 @@
 function showSigns() {
-  const signsArea = document.querySelectorAll('.horoscope-sign-wrapper a ');
+  const signsArea = document.querySelectorAll('.horoscope-sign-wrapper a');
   signsArea.forEach(signs => {
-    signs.addEventListener('click', e => {
-      e.preventDefault();
-      const nameSign = signs.querySelector('.sign-name');
-      const name = nameSign.textContent;
-      alert(`O signo selecionado foi:${name}`);
+    signs.addEventListener('click', event => {
+        event.stopPropagation();
+        event.preventDefault();
+        const nameSign = signs.querySelector('.sign-name');
+        const name = nameSign.textContent;
+        alert(`O signo selecionado foi:${name}`);
+ 
     });
   });
 }
